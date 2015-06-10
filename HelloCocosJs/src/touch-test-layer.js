136b309abdaa41613bd8852b9ec7d45545bce5e4
Xiaobin Li <xiaobin@xl.local>
2015-06-10 23:04:50 +0800

    竖屏；背景贴图；前景拖拽

--- HelloCocosJs/src/touch-test-layer.js
+++ HelloCocosJs/src/touch-test-layer.js
@@ -1,13 +1,57 @@
-var TouchTestLayer = cc.Layer.extend({
-    _drawNode:null,
-    _touchListener:null,
-    _lastPos:null,
+var TILE_WIDTH  = 99;
+var TILE_HEIGHT = 171;
 
-    ctor:function() {
+function getGameRect() {
+    var rect = cc.view.getViewPortRect();
+    rect.x -= TILE_WIDTH;
+    rect.y -= TILE_HEIGHT;
+    rect.width += 2 * TILE_WIDTH;
+    rect.height += + 2 * TILE_HEIGHT;
+    return rect;
+}
+
+var BackgroundLayer = cc.Layer.extend({
+    ctor : function() {
         this._super();
 
         var bg =  cc.LayerColor.create(cc.color(128, 128, 128, 225));
         this.addChild(bg);
+
+        var rect = getGameRect();
+        cc.log("game rect: {{" + rect.x + ", " + rect.y + "}, {" + rect.width + ", " + rect.height + "}}.");
+        for (var i=rect.x; i<rect.x+rect.width; i=i+TILE_WIDTH) {
+            for (var j=rect.y; j<rect.y+rect.height; j=j+TILE_HEIGHT) {
+                var pos = cc.p(i, j);
+                var tile = cc.Sprite.create(res.pic_jpg);
+                tile.setAnchorPoint(0, 0);
+                cc.log("add sprite at {" + pos.x + ", " + pos.y + "}.");
+                tile.setPosition(pos);
+                this.addChild(tile);
+            }
+        }
+
+        return true;
+    }
+});
+
+var TouchTestLayer = cc.Layer.extend({
+    _drawNode : null,
+    _touchListener : null,
+    _lastPos : null,
+    _bg : null,
+
+    _moveBackground : function(off) {
+        var pos = _bg.getPosition();
+        _bg.setPosition(cc.p(pos.x + off.x, pos.y + off.y));
+    },
+
+    ctor:function() {
+        this._super();
+
+        //var bg =  cc.LayerColor.create(cc.color(128, 128, 128, 225));
+        //this.addChild(bg);
+        _bg = new BackgroundLayer();
+        this.addChild(_bg);
 
         _drawNode = cc.DrawNode.create();
         this.addChild(_drawNode);
@@ -33,45 +77,57 @@ var TouchTestLayer = cc.Layer.extend({
         menu.y = 0;
         this.addChild(menu, 1);
 
+        cc.log("this=" + this);
         return true;
     },
 
     onEnter:function() {
         this._super();
 
+        cc.log("this=" + this);
+        cc.log(this._moveBackground);
+        this._moveBackground(cc.p(200, 200));
         _touchListener = cc.EventListener.create({
             event: cc.EventListener.TOUCH_ONE_BY_ONE,
             swallowTouches: true,
-            onTouchBegan: function(touch, event) {
-                _drawNode.clear();
-                var pos = touch.getLocation();
-                _drawNode.drawDot(pos, 10.0, cc.color(255, 0, 0, 255));
-                _lastPos = pos;
-                return true;
-            },
-            onTouchMoved: function(touch, event) {
-                var pos = touch.getLocation();
-                _drawNode.drawSegment(_lastPos, pos, 10.0, cc.color(255, 0, 0, 255));
-                _lastPos = pos;
-            },
-            onTouchEnded: function(touch, event) {
-                var pos = touch.getLocation();
-                _drawNode.drawDot(pos, 10.0, cc.color(255, 0, 0, 255));
-                _lastPos = 0;
-            }
+            onTouchBegan : this.onTouchBegan,
+            onTouchMoved : this.onTouchMoved,
+            onTouchEnded : this.onTouchEnded,
+            moveBackground : this._moveBackground
         });
 
         cc.eventManager.addListener(_touchListener, this);
     },
 
-    onExit:function() {
+    onExit : function() {
         cc.eventManager.removeListener(_touchListener);
         this._super();
+    },
+    onTouchBegan: function(touch, event) {
+        _drawNode.clear();
+        var pos = touch.getLocation();
+        _drawNode.drawDot(pos, 10.0, cc.color(255, 0, 0, 255));
+        _lastPos = pos;
+        return true;
+    },
+    onTouchMoved: function(touch, event) {
+        var pos = touch.getLocation();
+        cc.log(_drawNode);
+        cc.log(this._moveBackground);
+        _drawNode.drawSegment(_lastPos, pos, 10.0, cc.color(255, 0, 0, 255));
+        if (_lastPos) this.moveBackground(cc.p(pos.x-_lastPos.x, pos.y-_lastPos.y));
+        cc.log("this=" + this);
+        _lastPos = pos;
+    },
+    onTouchEnded: function(touch, event) {
+        var pos = touch.getLocation();
+        _drawNode.drawDot(pos, 10.0, cc.color(255, 0, 0, 255));
+        _lastPos = null;
     }
 });
 
 var TouchTestScene = cc.Scene.extend({
-    ctor:function() {
+    ctor : function() {
         this._super();
         this.addChild(new TouchTestLayer());
     }
